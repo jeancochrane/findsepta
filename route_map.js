f25a1d0599b356cef468f6ad68690989249c287f
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2016-07-07 18:24:45 -0400

    small bugfixes

--- route_map.js
+++ route_map.js
@@ -1,8 +1,12 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiamVhbmNvY2hyYW5lIiwiYSI6ImNpaDJndHlsMzB4cXN2a201bXdzZWxqZ2wifQ.toP9rJQ4ap-Z2chY_a87Vw';
 
+$(function() {
+
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v9',
     center: [-75.156133, 39.944918],
     zoom: 10.5
+});
+
 });
