33a312c6ae8d443585f2197712cce44837e6d285
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2016-07-07 18:58:27 -0400

    no more php

--- route_map.js
+++ route_map.js
@@ -1,12 +1,19 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiamVhbmNvY2hyYW5lIiwiYSI6ImNpaDJndHlsMzB4cXN2a201bXdzZWxqZ2wifQ.toP9rJQ4ap-Z2chY_a87Vw';
 
 $(function() {
+    $("form").submit(function(e) {
+        console.log($(this).serializeArray());
+        display_map();
+        e.preventDefault();
+    });
 
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    center: [-75.156133, 39.944918],
-    zoom: 10.5
-});
-
+    function display_map(){
+    // map initialization
+        var map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v9',
+            center: [-75.156133, 39.944918],
+            zoom: 10.5
+        });
+    }
 });
