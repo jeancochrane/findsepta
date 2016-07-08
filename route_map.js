c85b91926298e8de42a37bffcde640b8eb732b51
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2016-07-08 00:13:41 -0400

    api call working

--- route_map.js
+++ route_map.js
@@ -1,19 +1,53 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiamVhbmNvY2hyYW5lIiwiYSI6ImNpaDJndHlsMzB4cXN2a201bXdzZWxqZ2wifQ.toP9rJQ4ap-Z2chY_a87Vw';
 
 $(function() {
-    $("form").submit(function(e) {
-        console.log($(this).serializeArray());
-        display_map();
-        e.preventDefault();
-    });
+    var map;
+    $("form").submit(form_submit);
 
-    function display_map(){
+
+    function form_submit(e) {
+        e.preventDefault();
+        // get route selection from form
+        selected_routes = $(this).serializeArray();
+        console.log(selected_routes);
+        show_map();
+
+        //TODO: consolidate trolleys/buses
+        $.each(selected_routes, function(i,val) {
+            add_route(val.value)
+            //add trolleys and buses to map
+        });
+    }
+
+    function add_route(route) {
+        //add buses from a route to the map
+        $.getJSON("http://www3.septa.org/api/TransitView/index.php?route=" + route +"&callback=?", function(data) {
+            $.each(data.bus, function(i,val) {
+                var bus = val;
+                var sourceObj =  new mapboxgl.GeoJSONSource({
+                    data: {
+                       "type": "Point",
+                       "coordinates": [bus.lon, bus.lat]
+                    }
+                });
+                console.log(bus);
+                map.addSource(bus.label, sourceObj);
+                //TODO: figure out styling to display a marker for buses
+            });
+
+        });
+
+    }
+
+    function show_map(){
     // map initialization
-        var map = new mapboxgl.Map({
+        map = new mapboxgl.Map({
             container: 'map',
             style: 'mapbox://styles/mapbox/streets-v9',
-            center: [-75.156133, 39.944918],
+            center: [-75.156133, 39.944918], //philly!
             zoom: 10.5
         });
     }
+
+
 });
