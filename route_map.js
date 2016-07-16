342c03ecd5699220af31c39fcda9b8b89bcf2164
Jean Cochrane <jean@jeancochrane.com>
2016-07-16 14:53:58 -0400

    Trying to get icons to work
    Spent a while fiddling with icons… no success yet. This commit sets it
    so that inbound/outbound show up as blue and orange circles for now.

--- route_map.js
+++ route_map.js
@@ -7,19 +7,20 @@ $(function() {
     function form_submit(e) {
         e.preventDefault();
         // get route selection from form
-        selected_routes = $(this).serializeArray();
-        console.log(selected_routes);
+        selected_route = $("select").val();
+        console.log(selected_route);
         show_map();
         //TODO: consolidate trolleys/buses
-        //map.on('load', function() {
-            $.each(selected_routes, function(i,val) {
-                add_route(val.value)
-                //add trolleys and buses to map
-            });
-        //});
+        map.on('load', function() {
+            // nav controls
+            map.addControl(new mapboxgl.Navigation({position: 'top-left'}));
+            add_route(selected_route);
+            // show vehicle data on click
+            //map.on('click', show_data(e));    
+        });       
         // re-style form and move it away from the map
         $('.container').addClass('postSubmit');
-    }
+    };
 
     function add_route(route) {
         //add buses from a route to the map
@@ -43,41 +44,52 @@ $(function() {
                 console.log(bus);
                 map.addSource(bus.label, sourceObj);
                 if ((bus.Direction == 'NorthBound') || (bus.Direction == 'EastBound')) {
-                    console.log("Condition met!");
                     map.addLayer({
                         "id": bus.label,
-                        "type": "symbol",
+                        "type": "circle",
                         "source": bus.label,
-                        "layout": {
-                            "icon-image": "{icon}-15",
-                        },
                         "paint": {
-                            "icon-color": "#FF3F60"
+                            "circle-color": "#3C48A0",
+                            "circle-radius": 5
                         }
                     });
                 } else {
                     map.addLayer({
                         "id": bus.label,
-                        "type": "symbol",
+                        "type": "circle",
                         "source": bus.label,
-                        "layout": {
-                            "icon-image": "{icon}-15",
+                        "paint": {
+                            "circle-color": "#EBA255",
+                            "circle-radius": 5
                         }
                     });
                 }
             });
         });
-    }
+    };
 
     function show_map() {
     // map initialization
         map = new mapboxgl.Map({
             container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v9',
+            style: 'mapbox://styles/mapbox/light-v9',
             center: [-75.156133, 39.944918], //philly!
             zoom: 10.5
         });
+    };
+
+    // currently commented out in code - needs work
+    function show_data(e) {
+        e.preventDefault();
+        var mouse_coordinates = [e.lngLat.lng, e.lngLat.lat];
+        var bus_data = map.queryRenderedFeatures(e.point, {
+            layers: "bus"
+        });
+        console.log(JSON.stringify(bus_data));
+        var popup = new mapboxgl.Popup()
+                .setLngLat(mouse_coordinates)
+                .setHTML('<h3>' + censusObj['NAMELSAD10'] + '</h3>'
+                        + '<p>' + censusObj['PERCENT_WHITE'] + '</p>')
+                .addTo(map);
     }
-
-
 });
